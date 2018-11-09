4e1fbd31d83e8c3f210069221c2c597552f26893
thomastognacci <thomas.tognacci@gmail.com>
2018-11-08 23:26:47 -0330

    connected to prisma, end video 14

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,3 +1,15 @@
-const Query = {};
+const {forwardTo} = require("prisma-binding");
+
+const Query = {
+  items: forwardTo("db"),
+  // async items(parent, args, ctx, info) {
+  //   const items = await ctx.db.query.items();
+  //   return items;
+  // },
+  // dogs: function(parent, args, ctx, info) {
+  //   global.dogs = global.dogs || [];
+  //   return global.dogs;
+  // },
+};
 
 module.exports = Query;
