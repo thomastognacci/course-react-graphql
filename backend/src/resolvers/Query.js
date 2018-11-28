6b47d347ec6917f6a057357a4848eae518e5fe28
thomastognacci <thomas.tognacci@gmail.com>
2018-11-28 11:13:01 -0330

    fixed dependencies vulnerabilities

--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,11 +1,8 @@
-const {forwardTo} = require("prisma-binding");
-
 const Query = {
-  items: forwardTo("db"),
-  // async items(parent, args, ctx, info) {
-  //   const items = await ctx.db.query.items();
-  //   return items;
-  // },
+  async items(parent, args, ctx, info) {
+    const items = await ctx.db.query.items();
+    return items;
+  },
   // dogs: function(parent, args, ctx, info) {
   //   global.dogs = global.dogs || [];
   //   return global.dogs;
